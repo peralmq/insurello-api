6a25a894c20bd74aba33352dad2664f406067381
Pelle Almquist <per.almquist@gmail.com>
2017-10-07 17:02:08 +0200

    Rename app to server

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,15 @@ import * as express from 'express'
 import * as morgan from 'morgan'
 // import Storage from './storage'
 
-const app: express.Application = express()
-app.use(morgan('dev'))
+const server: express.Application = express()
+server.use(morgan('dev'))
 
-app.all(
+server.all(
     '/',
     (req: express.Request, res: express.Response) => res.send('Hello from the TypeScript world!')
 )
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Express is up and running.`))
+server.listen(port, () => console.log(`Express is up and running.`))
 
-export default app
+export default server
